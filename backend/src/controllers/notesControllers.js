255d7dedd567da14160a86eb6f704d3289d53cab
PrithwishDas101 <prithwish6636@gmail.com>
2026-05-07 23:48:33 +0530

    updated architecture of codebase and added crontrollers and routes

---
+++ backend/src/controllers/notesControllers.js
@@ -0,0 +1,15 @@
+export const getAllNotes = (req, res) => {
+    res.status(200).send("you have 20 notes")
+}
+
+export const createNote = (req, res) => {
+    res.status(201).json({message:`note created succesfully`})
+}
+
+export const updateNote = (req, res) => {
+    res.status(200).json({message:`note updated succesfully`})
+}
+
+export const deleteNote = (req, res) => {
+    res.status(200).json({message:`note deleted succesfully`})
+}
