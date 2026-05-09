a848b62e7135a28c5045a54d43d8b9d0d57c6785
PrithwishDas101 <prithwish6636@gmail.com>
2026-05-09 22:24:27 +0530

    frontend set up complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="forest" className="min-h-screen bg-base-100">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
